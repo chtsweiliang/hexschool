bb9a56997642cc46a2adc65535a62a56b82401ed
chtsweiliang <chtsweiliang@gmail.com>
2020-07-06 11:41:29 +0800

    finish week3 homework
    第三週主線任務

--- README.md
+++ README.md
@@ -72,3 +72,60 @@ if (sayYouLoveMe()) { console.log('說你愛我') };
 - LV1：建立自己的資料內容，並且前端可取得資料即算完成（僅需要使用 `console.log` 呈現）
 - LV2：使用課程版型，並且完整呈現列表
 - LV3：依據自己的主題，建立合適的列表版型
+
+# 第三週主線任務 - 產品管理頁面
+第三週開始我們會開始接觸 Vue.js，在此階段同學可多練習 Vue.js 的各項語法及指令，目標先以完成一頁式的產品新增、刪除、修改的頁面（先不串接 API）。
+
+![](https://i.imgur.com/oBgmStb.png)
+
+![](https://i.imgur.com/pqo5PuX.jpg)
+
+
+- 範例頁面 gh-page：https://hexschool.github.io/js-training-task/
+- 範例程式碼 GitHub：https://github.com/hexschool/js-training-task
+
+### 使用者故事
+
+- 可以新增、編輯、刪除商品
+- 商品啟用、關閉可以使用不同的顏色標示
+- 商品欄位如下（請注意型別）
+	- title：商品名稱 - string
+	- category：商品分類 - string
+	- content：商品敘述 - string
+	- description：商品說明 - string
+	- imageUrl：商品圖片 - string
+	- enabled：是否上架 - boolean
+	- origin_price：原價 - number(integer)
+	- price：售價 - number(integer)
+	- unit：單位 - string
+
+### 任務說明：
+- 依據使用者需求完成此作業
+
+課程任務均有提供完整範例程式碼，如果開發上不是很順利可以參考範例程式碼來撰寫。
+使用範例程式碼注意事項：
+- **禁止直接使用範例程式碼提交**
+- 如欲參考範例程式碼，請避免複製貼上，請自行重新撰寫
+- 參考範例程式碼，請在繳交作業時附註：「本作業有參考範例程式碼」
+- 參考範例程式碼，請在原始碼內補上註解，讓自己加深學習印象
+```js
+// 當判斷式接收到 sayYouLoveMe() 回傳 true 的結果時，則會運行接下來的陳述片段
+if (sayYouLoveMe()) { console.log('說你愛我') };
+```
+
+### 提交難度挑戰：
+- LV1：參考範例程式碼，並重新撰寫及補上註解（禁止複製範例程式碼）
+- LV2：重新撰寫產品管理頁面
+- LV3：產品內新增一個物件欄位 options，新增更多不同的設定在其中
+
+options 範例：
+```
+options: {
+  stars: 5,
+  comments: [{
+    name: '小明',
+    avator: 'img url...',
+    comment: '漂亮阿姨的餃子最好吃了'
+  }]
+}
+```
---
+++ week3/all.js
@@ -0,0 +1,112 @@
+new Vue({
+  el: '#app',
+  data: {
+    products: [
+      {
+        id: 1586934917210,
+        unit: '箱',
+        category: '荔枝',
+        title: '陳家果園玉荷包禮盒',
+        origin_price: 800,
+        price: 600,
+        description: '陳家果園玉荷包禮盒',
+        content: '玉荷包禮盒5台斤裝',
+        is_enabled: 1,
+        imageUrl: 'https://images.unsplash.com/photo-1591600986686-bf5f872d2e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        options: {
+          id: 1,
+          stars: 5,
+          comments: [{
+            name: '小明',
+            avator: 'img url...',
+            comment: '漂亮阿姨的餃子最好吃了'
+          }]
+        },
+      },
+      {
+        id: 1196934917910,
+        unit: '箱',
+        category: '芭樂',
+        title: '陳家果園珍珠芭樂',
+        origin_price: 600,
+        price: 350,
+        description: '無毒珍珠芭樂5台斤(小箱)',
+        content: '無毒珍珠芭樂5台斤(小箱)',
+        is_enabled: 0,
+        imageUrl: 'https://images.unsplash.com/photo-1536511132770-e5058c7e8c46?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        options: {
+          id: 2,
+          stars: 5,
+          comments: [{
+            name: '小美',
+            avator: 'img url...',
+            comment: '漂亮小姐的餃子最好吃了'
+          }]
+        },
+      },
+    ],
+    tempProduct: {
+      imageUrl: [],
+    },
+    tempComment: {
+
+    },
+  },
+
+  methods: {
+    openModal(type, item) {
+      switch (type) {
+        case 'create':
+          this.tempProduct = {
+            imageUrl: [],
+          };
+          $('#productModal').modal('show');
+          break;
+        case 'edit':
+          this.tempProduct = Object.assign({}, item);
+          $('#productModal').modal('show');
+          break;
+        case 'delete':
+          this.tempProduct = Object.assign({}, item);
+          $('#delProductModal').modal('show');
+          break;
+        case 'comment':
+          this.tempComment = Object.assign({}, item);
+          $('#commetProductModal').modal('show');
+          break;
+        default:
+          break;
+      }
+    },
+    updateProduct() {
+      let i;
+      if (this.tempProduct.id) {
+        this.products.forEach((item, index) => {
+          if (item.id == this.tempProduct.id) {
+            i = index;
+          }
+        });
+        this.$set(this.products, i, this.tempProduct);
+      } else {
+        if (this.tempProduct.title) {
+          const id = (new Date()).getTime();//取 unix 時間
+          this.tempProduct.id = id;
+          this.products.push(this.tempProduct);
+        }
+      }
+      this.tempProduct = {};
+      $('#productModal').modal('hide');
+    },
+    delProduct() {
+      if (this.tempProduct.id) {
+        this.products.forEach((item, i) => {
+          if (item.id === this.tempProduct.id) {
+            this.products.splice(i, 1);
+            this.tempProduct = {};
+          }
+        });
+      }
+      $('#delProductModal').modal('hide');
+    },
+  },
+});
